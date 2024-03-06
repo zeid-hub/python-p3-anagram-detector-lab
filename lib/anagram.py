a055104e5f92d94e4b57f66bf58d7bc0b77ee007
zeid <zeidhassan4@gmail.com>
2024-03-06 17:21:33 +0300

    Done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+#!/usr/bin/env python3
+class Anagram:
+    def  __init__(self, word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(word))
+
+    def match(self, words):
+        "Returns a list of words that match the initialized word."
+        return [word for word in words if self.sorted_word in ''.join(sorted(word)) == self.sorted_word]
